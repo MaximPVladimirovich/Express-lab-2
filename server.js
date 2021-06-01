f35df2f36aebac60f9a136bfb24631c12ee37add
MaximPVladimirovich <maxim.bleeker@gmail.com>
2021-06-01 18:17:44 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require(`express`);
 const app = express();
 const port = 3000;
 
-
+// This will display the name from the url.
 app.get(`/greeting/:name`, (req, res) => {
     res.send(`Hello ${req.params.name}!`)
+})
+
+// Tip calculator 
+app.get(`/tip/:total/:percentage`, (req, res) => {
+    let multiplier = 0.01;
+    let total = parseInt(req.params.total);
+    let percentage = parseInt(req.params.percentage) * multiplier;
+    console.log(total)
+    res.send(`Your tip should be $${total * percentage}`)
 })
 
 app.listen(port)
