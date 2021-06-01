676426ea6824ad28cee0eb4f2fc31de8576cfba1
MaximPVladimirovich <maxim.bleeker@gmail.com>
2021-06-01 17:58:02 -0500

    Greeting express application created.

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require(`express`);
+const app = express();
+const port = 3000;
+
+
+app.get(`/greeting/:name`, (req, res) => {
+    res.send(`Hello ${req.params.name}!`)
+})
+
+app.listen(port)
