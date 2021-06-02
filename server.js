ebca6619b73cc24353b460c0b3f5229e60ecf550
Maxim <Maxim.bleeker@gmail.com>
2021-06-01 20:42:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,8 @@ const app = express();
 const port = 3000;
 
 // Magic 8 ball
-
 let answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-
 let random = Math.floor(Math.random() * answers.length)
-
 app.get(`/magic/:question`, (req, res) => {
     let question = req.params.question;
     res.send(`${question} <h1> ${answers[random]}</h1>`)
